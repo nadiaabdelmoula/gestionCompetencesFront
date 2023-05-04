0bf4946e5d27d11d8c6b915336f69d7702ff59bf
nadiaabdelmoula <nadia.abdelmoula@esprit.tn>
2023-05-04 03:07:32 -0700

    ok

--- lib/services/projetService.dart
+++ lib/services/projetService.dart
@@ -1,98 +1,93 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 
-
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/projectModel.dart';
 import '../widgets/alertDialog.dart';
-class ProjetService{
+
+class ProjetService {
   var baseUrl = "http://localhost:5000/projet/";
-  Future<void> addProjet(String dure,String titre,String chef,String description,String client, BuildContext context) async {
-    var url = "http://localhost:8000/addprojet -H 'Authorization: Basic cm9vdDpyb290'";
-    Map<String,String> params = {
+  Future<void> addProjet(String dure, String titre, String chef,
+      String description, String client, BuildContext context) async {
+    var url = baseUrl + "add";
+    Map<String, String> params = {
       "titre": titre,
       "etat": "regie",
       "duree": dure,
       "description": description,
-      "chef":chef,
-      "client":client,
+      "chef": chef,
+      "client": client,
     };
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString("jwt-token");
-    var response = await http.post(Uri.parse(url).replace(queryParameters: params),
+    var response = await http.post(
+      Uri.parse(url).replace(queryParameters: params),
       headers: <String, String>{
         "Content-Type": "application/json",
-        "Authorization":"Bearer "+token!,
-      },);
-    Map<String,dynamic> responseJson = await jsonDecode(response.body);
-    if(response.statusCode==200)
-      {
-        final snackBar = SnackBar(content: Text("Projet :"+titre+" added with success"),);
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      }
-    else {
-
-      showDialog(context: context,
+        "Authorization": "Bearer " + token!,
+      },
+    );
+    Map<String, dynamic> responseJson = await jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      final snackBar = SnackBar(
+        content: Text("Projet :" + titre + " added with success"),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      showDialog(
+          context: context,
           builder: (BuildContext context) {
             return AdvanceCustomAlert(responseJson["message"]);
           });
     }
   }
-  Future<List<Projet>> getProjects()async{
+
+  Future<List<Projet>> getProjects() async {
     List<Projet> projects = [];
-    var url = "http://localhost:8000/listprojet -H 'Authorization: Basic cm9vdDpyb290'";
+    var url = baseUrl + "list";
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString("jwt-token");
 
+    var response = await http.get(
+      Uri.parse(url),
+      headers: <String, String>{
+        "Authorization": "Bearer " + token!,
+      },
+    );
 
-    var response = await http.get(Uri.parse(url),
-      headers: <String,String>{
-
-        "Authorization":"Bearer "+token!,
-
-      },);
-
-    if(response.statusCode == 200)
-    {
+    if (response.statusCode == 200) {
       var jsons = jsonDecode(response.body);
 
-  print(jsons);
-      for(var json in jsons)
-      {
+      print(jsons);
+      for (var json in jsons) {
         projects.add(Projet.fromJson(json));
-
       }
       print(projects);
-
-    }
-
-    else {
+    } else {
       var responseJson = jsonDecode(response.body.toString());
       print(responseJson["message"]);
     }
     print("bbbbbbb");
 
-
     return projects;
   }
-  Future<List<Projet>> getProjectsRegie(String search)async{
+
+  Future<List<Projet>> getProjectsRegie(String search) async {
     List<Projet> projects = [];
-    var url = "http://localhost:8000/listprojet -H 'Authorization: Basic cm9vdDpyb290'";
+    var url = "http://localhost:5000/projet/list";
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString("jwt-token");
 
+    var response = await http.get(
+      Uri.parse(url),
+      headers: <String, String>{
+        "Authorization": "Bearer " + token!,
+      },
+    );
 
-    var response = await http.get(Uri.parse(url),
-      headers: <String,String>{
-
-        "Authorization":"Bearer "+token!,
-
-      },);
-
-    if(response.statusCode == 200)
-    {
+    if (response.statusCode == 200) {
       var jsons = jsonDecode(response.body);
 
       print("jsons");
@@ -105,8 +100,7 @@ class ProjetService{
             projects.add(Projet.fromJson(json));
           }
         }
-      }
-      else {
+      } else {
         print("case 3");
         for (var json in jsons) {
           String etat = json["etat"];
@@ -116,41 +110,36 @@ class ProjetService{
 
           if (etat.compareTo("regie") == 0) {
             print("case 2--------------");
-            if((client.contains(search.toUpperCase())==true)||(manager.contains(search.toUpperCase())==true)||(titre.contains(search.toUpperCase())==true))
-            {
+            if ((client.contains(search.toUpperCase()) == true) ||
+                (manager.contains(search.toUpperCase()) == true) ||
+                (titre.contains(search.toUpperCase()) == true)) {
               projects.add(Projet.fromJson(json));
             }
-
           }
         }
       }
-
-
-    }
-
-    else {
+    } else {
       var responseJson = jsonDecode(response.body.toString());
       print(responseJson["message"]);
     }
     print("bbbbbbb");
 
-
     return projects;
   }
-  Future<List<Projet>> getProjectsForfait(String search)async{
+
+  Future<List<Projet>> getProjectsForfait(String search) async {
     List<Projet> projects = [];
-    var url = "http://localhost:8000/listprojet -H 'Authorization: Basic cm9vdDpyb290'";
+    var url = baseUrl + "list";
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString("jwt-token");
-    var response = await http.get(Uri.parse(url),
-      headers: <String,String>{
+    var response = await http.get(
+      Uri.parse(url),
+      headers: <String, String>{
+        "Authorization": "Bearer " + token!,
+      },
+    );
 
-        "Authorization":"Bearer "+token!,
-
-      },);
-
-    if(response.statusCode == 200)
-    {
+    if (response.statusCode == 200) {
       var jsons = jsonDecode(response.body);
 
       print("jsons");
@@ -163,8 +152,7 @@ class ProjetService{
             projects.add(Projet.fromJson(json));
           }
         }
-      }
-      else {
+      } else {
         print("case 3");
         for (var json in jsons) {
           String etat = json["etat"];
@@ -174,49 +162,44 @@ class ProjetService{
 
           if (etat.compareTo("forfait") == 0) {
             print("case 2--------------");
-          if((client.contains(search.toUpperCase())==true)||(manager.contains(search.toUpperCase())==true)||(titre.contains(search.toUpperCase())==true))
-            {
+            if ((client.contains(search.toUpperCase()) == true) ||
+                (manager.contains(search.toUpperCase()) == true) ||
+                (titre.contains(search.toUpperCase()) == true)) {
               projects.add(Projet.fromJson(json));
             }
-
           }
         }
       }
-
-
-    }
-
-    else {
+    } else {
       var responseJson = jsonDecode(response.body.toString());
       print(responseJson["message"]);
     }
 
-
-
     return projects;
   }
-  Future<List<Projet>> getProjectsRegieByManager(String search)async{
+
+  Future<List<Projet>> getProjectsRegieByManager(String search) async {
     List<Projet> projects = [];
-    var url = "http://localhost:8000/listprojet -H 'Authorization: Basic cm9vdDpyb290'";
+    var url = baseUrl + "list";
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString("jwt-token");
-    String? userName =prefs.getString("user");
-    String? m =prefs.getString("manager");
+    String? userName = prefs.getString("user");
+    String? m = prefs.getString("manager");
     String value;
-    if(m!.compareTo("no manager")==0){value = userName!;}
-    else{value = m;}
+    if (m!.compareTo("no manager") == 0) {
+      value = userName!;
+    } else {
+      value = m;
+    }
 
+    var response = await http.get(
+      Uri.parse(url),
+      headers: <String, String>{
+        "Authorization": "Bearer " + token!,
+      },
+    );
 
-    var response = await http.get(Uri.parse(url),
-      headers: <String,String>{
-
-        "Authorization":"Bearer "+token!,
-
-      },);
-
-
-    if(response.statusCode == 200)
-    {
+    if (response.statusCode == 200) {
       var jsons = jsonDecode(response.body);
 
       print("jsons");
@@ -228,12 +211,12 @@ class ProjetService{
 
           if (etat.compareTo("regie") == 0) {
             print("case 2");
-            if(manager.toUpperCase().compareTo(value.toUpperCase())==0){projects.add(Projet.fromJson(json));}
-
+            if (manager.toUpperCase().compareTo(value.toUpperCase()) == 0) {
+              projects.add(Projet.fromJson(json));
+            }
           }
         }
-      }
-      else {
+      } else {
         print("case 3");
         for (var json in jsons) {
           String etat = json["etat"].toString();
@@ -244,54 +227,45 @@ class ProjetService{
           if ((etat.compareTo("regie") == 0)) {
             print("case 2--------------");
 
-            if((manager.compareTo(value.toString().toUpperCase())==0)){
-              if((client.contains(search.toUpperCase())==true)||(titre.contains(search.toUpperCase())==true))
-              {
+            if ((manager.compareTo(value.toString().toUpperCase()) == 0)) {
+              if ((client.contains(search.toUpperCase()) == true) ||
+                  (titre.contains(search.toUpperCase()) == true)) {
                 projects.add(Projet.fromJson(json));
               }
             }
-
-
           }
         }
       }
-
-
-    }
-
-    else {
+    } else {
       var responseJson = jsonDecode(response.body.toString());
       print(responseJson["message"]);
     }
 
-
-
     return projects;
   }
 
-
-
-
-  Future<List<Projet>> getProjectsForfaitByManager(String search)async{
+  Future<List<Projet>> getProjectsForfaitByManager(String search) async {
     List<Projet> projects = [];
-    var url = "http://localhost:8000/listprojet -H 'Authorization: Basic cm9vdDpyb290'";
+    var url = baseUrl + "list";
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString("jwt-token");
-    String? userName =prefs.getString("user");
-    String? m =prefs.getString("manager");
+    String? userName = prefs.getString("user");
+    String? m = prefs.getString("manager");
     String value;
-    if(m!.compareTo("no manager")==0){value = userName!;}
-    else{value = m;}
+    if (m!.compareTo("no manager") == 0) {
+      value = userName!;
+    } else {
+      value = m;
+    }
 
-    var response = await http.get(Uri.parse(url),
-      headers: <String,String>{
+    var response = await http.get(
+      Uri.parse(url),
+      headers: <String, String>{
+        "Authorization": "Bearer " + token!,
+      },
+    );
 
-        "Authorization":"Bearer "+token!,
-
-      },);
-
-    if(response.statusCode == 200)
-    {
+    if (response.statusCode == 200) {
       var jsons = jsonDecode(response.body);
 
       print("jsons");
@@ -303,12 +277,12 @@ class ProjetService{
 
           if (etat.compareTo("forfait") == 0) {
             print("case 2");
-            if(manager.toUpperCase().compareTo(value.toUpperCase())==0){projects.add(Projet.fromJson(json));}
-
+            if (manager.toUpperCase().compareTo(value.toUpperCase()) == 0) {
+              projects.add(Projet.fromJson(json));
+            }
           }
         }
-      }
-      else {
+      } else {
         print("case 3");
         for (var json in jsons) {
           String etat = json["etat"].toString();
@@ -319,44 +293,39 @@ class ProjetService{
           if ((etat.compareTo("forfait") == 0)) {
             print("case 2--------------");
 
-            if((manager.compareTo(value.toString().toUpperCase())==0)){
-              if((client.contains(search.toUpperCase())==true)||(titre.contains(search.toUpperCase())==true))
-              {
+            if ((manager.compareTo(value.toString().toUpperCase()) == 0)) {
+              if ((client.contains(search.toUpperCase()) == true) ||
+                  (titre.contains(search.toUpperCase()) == true)) {
                 projects.add(Projet.fromJson(json));
               }
             }
-
-
           }
         }
       }
-
-
-    }
-
-    else {
+    } else {
       var responseJson = jsonDecode(response.body.toString());
       print(responseJson["message"]);
     }
 
-
-
     return projects;
   }
-  Future<Projet?> getProjectWithTitle(String titre)async{
-    var url = baseUrl+titre;
+
+  Future<Projet?> getProjectWithTitle(String titre) async {
+    var url = baseUrl + titre;
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString("jwt-token");
-   var response = await http.get(Uri.parse(url),headers: <String,String>{
-
-     "Authorization":"Bearer "+token!,
-
-   },);
-   if(response.statusCode == 200)
-     { Map<String, dynamic> responseJson = await jsonDecode(response.body.toString());
-       Projet p = Projet.fromJson(responseJson);
-       return p;
-     }
-
+    var response = await http.get(
+      Uri.parse(url),
+      headers: <String, String>{
+        "Authorization": "Bearer " + token!,
+      },
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> responseJson =
+          await jsonDecode(response.body.toString());
+      Projet p = Projet.fromJson(responseJson);
+      return p;
+    }
   }
 }
+
